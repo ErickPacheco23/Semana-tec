58f2b7d3a4572cba2b520bb9201b90a1f6538021
rodrigo <salinas>
2021-03-18 13:06:06 -0600

    Auto stash before merge of "main" and "ErickPacheco23/main"

--- simple_conv.py
+++ simple_conv.py
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 """
 By Abhisek Jana
 code taken from https://github.com/adeveloperdiary/blog/tree/master/Computer_Vision/Sobel_Edge_Detection
@@ -46,3 +47,54 @@ def convolution(image, kernel):
 imagen = cv2.imread('imagen.jpg', 0)
 cv2.imshow('image', imagen)
 cv2.waitKey(0)
+=======
+"""
+By Abhisek Jana
+code taken from https://github.com/adeveloperdiary/blog/tree/master/Computer_Vision/Sobel_Edge_Detection
+blog http://www.adeveloperdiary.com/data-science/computer-vision/how-to-implement-sobel-edge-detection-using-python-from-scratch/
+
+Modified by Benjamin Valdes
+"""
+
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+ 
+def conv_helper(fragment, kernel):
+    """ multiplica 2 matices y devuelve su suma"""
+    
+    f_row, f_col = fragment.shape
+    k_row, k_col = kernel.shape 
+    result = 0.0
+    for row in range(f_row):
+        for col in range(f_col):
+            result += fragment[row,col] *  kernel[row,col]
+    return result
+
+def convolution(image, kernel):
+    """Aplica una convolucion sin padding (valida) de una dimesion 
+    y devuelve la matriz resultante de la operación
+    """
+
+    image_row, image_col = image.shape #asigna alto y ancho de la imagen 
+    kernel_row, kernel_col = kernel.shape #asigna alto y ancho del filtro
+   
+    output = np.zeros(image.shape) #matriz donde guardo el resultado
+   
+    for row in range(image_row):
+        for col in range(image_col):
+                output[row, col] = conv_helper(
+                                    image[row:row + kernel_row, 
+                                    col:col + kernel_col],kernel)
+             
+    plt.imshow(output, cmap='gray')
+    plt.title("Output Image using {}X{} Kernel".format(kernel_row, kernel_col))
+    plt.show()
+ 
+    return output
+
+imagen = cv2.imread('imagen.jpg')
+cv2.imshow('image', imagen)
+#kernel = np.ones((3,3))
+#resultado = convolution(gray, kernel)
+>>>>>>> Stashed changes
